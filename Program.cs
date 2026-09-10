a62a28c6427a89110d1e138fd24ba56bf2e3cb0f
yaroslav-251 <kolpakov1803@mail.ru>
2026-09-10 22:01:55 +0300

    Lab02: numeric type ranges and overflow

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Границы целочисленных типов");
+Console.WriteLine($"byte:   {byte.MinValue} .. {byte.MaxValue}");
+Console.WriteLine($"short:  {short.MinValue} .. {short.MaxValue}");
+Console.WriteLine($"int:    {int.MinValue} .. {int.MaxValue}");
+Console.WriteLine($"long:   {long.MinValue} .. {long.MaxValue}");
+
+Console.WriteLine();
+Console.WriteLine("Границы дробных типов");
+Console.WriteLine($"float:   {float.MinValue} .. {float.MaxValue}");
+Console.WriteLine($"double:  {double.MinValue} .. {double.MaxValue}");
+Console.WriteLine($"decimal: {decimal.MinValue} .. {decimal.MaxValue}");
+
+Console.WriteLine();
+Console.WriteLine("Переполнение byte");
+
+byte maxByte = 255;
+byte overflowed = (byte)(maxByte + 1);
+Console.WriteLine($"255 + 1 для byte = {overflowed}");
